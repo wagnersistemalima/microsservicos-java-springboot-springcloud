9fc2e04cb6a15cdcc63c32920b0c66c3991cac21
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-06-14 16:03:17 -0300

    setup do microsservico folha de pagamento criado

---
+++ folha-pagamento/src/main/java/br/com/wagner/folhapagamento/FolhaPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.wagner.folhapagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolhaPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolhaPagamentoApplication.class, args);
+	}
+
+}
---
+++ folha-pagamento/src/test/java/br/com/wagner/folhapagamento/FolhaPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wagner.folhapagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
