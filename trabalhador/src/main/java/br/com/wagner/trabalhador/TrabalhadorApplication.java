fb66e057d70b119a4402ce092419491cace3ebd0
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-06-15 16:47:26 -0300

    configuracao eureka-clients

--- trabalhador/src/main/java/br/com/wagner/trabalhador/TrabalhadorApplication.java
+++ trabalhador/src/main/java/br/com/wagner/trabalhador/TrabalhadorApplication.java
@@ -2,8 +2,11 @@ package br.com.wagner.trabalhador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableEurekaClient
 @EnableSwagger2
 @SpringBootApplication
 public class TrabalhadorApplication {
