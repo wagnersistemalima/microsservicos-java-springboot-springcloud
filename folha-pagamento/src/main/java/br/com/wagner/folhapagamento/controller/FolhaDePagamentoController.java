e8d5d25f9cbe9abc2e5dc212f468e092076be8b0
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-06-15 18:10:01 -0300

    implementando Hystrix, e configurando time out

--- folha-pagamento/src/main/java/br/com/wagner/folhapagamento/controller/FolhaDePagamentoController.java
+++ folha-pagamento/src/main/java/br/com/wagner/folhapagamento/controller/FolhaDePagamentoController.java
@@ -3,6 +3,7 @@ package br.com.wagner.folhapagamento.controller;
 import br.com.wagner.folhapagamento.exception.ResourceNotFoundException;
 import br.com.wagner.folhapagamento.model.Pagamento;
 import br.com.wagner.folhapagamento.service.FolhaDePagamentoService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @RestController
@@ -25,6 +27,7 @@ public class FolhaDePagamentoController {
 
     // end point para consulta
 
+    @HystrixCommand(fallbackMethod = "consultaAlternativa")
     @GetMapping(value = "/{id}/dias/{dias}")
     public ResponseEntity<?> consulta(@PathVariable("id") Long id, @PathVariable("dias") Integer dias) {
         logger.info("iniciando consulta de pagamento");
@@ -37,5 +40,12 @@ public class FolhaDePagamentoController {
         return  ResponseEntity.ok().body(pagamento);
 
 
+    }
+
+    // end point alternativo
+
+    public ResponseEntity<?> consultaAlternativa(Long id, Integer dias) {
+        Pagamento pagamento = new Pagamento(id, "Brann", new BigDecimal(50.0), dias);
+        return  ResponseEntity.ok().body(pagamento);
     }
 }
