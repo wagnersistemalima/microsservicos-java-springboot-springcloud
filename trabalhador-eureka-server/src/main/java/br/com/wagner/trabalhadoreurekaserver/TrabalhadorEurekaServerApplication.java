e9f6d28f56a3239ac92c00e841268bab217df227
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-06-15 15:41:18 -0300

    setup do projeto trabalhador-eureka-server, pasta image

---
+++ trabalhador-eureka-server/src/main/java/br/com/wagner/trabalhadoreurekaserver/TrabalhadorEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.wagner.trabalhadoreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhadorEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhadorEurekaServerApplication.class, args);
+	}
+
+}
