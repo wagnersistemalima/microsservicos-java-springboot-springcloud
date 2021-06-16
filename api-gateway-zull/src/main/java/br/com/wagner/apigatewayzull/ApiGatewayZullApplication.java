c2058fc2f3b398167aeabc382e59137896425499
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-06-16 09:27:35 -0300

    projeto api-gateway-zull criado

---
+++ api-gateway-zull/src/main/java/br/com/wagner/apigatewayzull/ApiGatewayZullApplication.java
@@ -0,0 +1,13 @@
+package br.com.wagner.apigatewayzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayZullApplication.class, args);
+	}
+
+}
---
+++ api-gateway-zull/src/test/java/br/com/wagner/apigatewayzull/ApiGatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wagner.apigatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayZullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
